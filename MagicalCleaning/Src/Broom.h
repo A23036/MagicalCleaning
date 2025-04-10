90de24d3a382b9423d5a6c7bbfb31ec75a602332
A23036 <san-a23036@sist.ac.jp>
2025-04-10 09:21:08 +0900

    Broomクラスにコメント追加

--- MagicalCleaning/Src/Broom.h
+++ MagicalCleaning/Src/Broom.h
@@ -1,6 +1,11 @@
 #pragma once
 #include "Object3D.h"
 
+// ---------------------------------------------------------------------------
+// 箒処理
+// 
+// ゲームプレイ中にプレイヤーの持つ箒の処理を行うクラス
+// ---------------------------------------------------------------------------
 class Broom : public Object3D {
 public:
 	Broom(Object3D* parentModel, int num);
@@ -9,6 +14,6 @@ public:
 	void Draw() override;
 	void SetPos(MATRIX4X4 m);
 private:
-	Object3D* parent;
+	Object3D* parent;	//親のポインタ
 	MATRIX4X4 mat;
 };
