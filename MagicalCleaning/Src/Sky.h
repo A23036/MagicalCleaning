a7f2816e0743f03a8c5c0866462a66109b74415d
A23036 <san-a23036@sist.ac.jp>
2025-04-10 15:09:13 +0900

    Skyクラスの整理

--- MagicalCleaning/Src/Sky.h
+++ MagicalCleaning/Src/Sky.h
@@ -1,12 +1,15 @@
 #pragma once
 #include "Object3D.h"
 
+// ---------------------------------------------------------------------------
+// SkyBoxˆ—
+// ---------------------------------------------------------------------------
 class Sky : public Object3D {
 public:
 	Sky();
-	~Sky();
+	virtual ~Sky();
 	void Update() override;
 	void Draw() override;
 private:
-	float rotate;
+	float rotate; //‰ñ“]—p•Ï”
 };
